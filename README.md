2e7f43d85eec1bc556bf01e180fdd31f21b9fbca
Zahro332 <139197960+Zahro332@users.noreply.github.com>
2023-08-13 17:16:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-5h5nrn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-rbyoah)
---
+++ index.ts
@@ -0,0 +1,13 @@
+//#
+let myNumber1, myNumber2: number;
+let myNumber3: number;
+
+myNumber1 = 55;
+myNumber2 = 35;
+myNumber3 = myNumber1;
+myNumber1 = myNumber2;
+myNumber2 = myNumber3;
+
+console.log(myNumber1);
+console.log(myNumber2);
+
